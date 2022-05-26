1e67a111a4f1727a132386ba3197dc9206ef2fee
程序员欣宸 <zq2599@gmail.com>
2022-05-26 00:15:38 +0000

    新增SpringBoot的demo

---
+++ helloworld/src/test/java/com/bolingcavalry/helloworld/HelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bolingcavalry.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
