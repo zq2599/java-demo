dfb92cfed922588cab9fa2fbe3c69070af432241
程序员欣宸 <zq2599@gmail.com>
2022-06-03 03:51:08 +0000

    修改JDK版本，增加单元测试类

--- helloworld/src/test/java/com/bolingcavalry/helloworld/HelloworldApplicationTests.java
+++ helloworld/src/test/java/com/bolingcavalry/helloworld/HelloworldApplicationTests.java
@@ -1,13 +1,27 @@
 package com.bolingcavalry.helloworld;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class HelloworldApplicationTests {
 
 	@Test
-	void contextLoads() {
-	}
+    void testHello(@Autowired MockMvc mvc) throws Exception {
+        // 模拟
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello World")))
+				.andDo(print());
+    }
 
 }
